3bc2632a8c93b3d27964f4be297b1768a24dd121
CryoSlayer <npetr9395@gmail.com>
2026-02-15 13:59:28 +0300

    Update GetLavender.ps1

--- GetLavender.ps1
+++ GetLavender.ps1
@@ -1,4 +1,3 @@
-# LAVENDER-WOA BOOTSTRAPPER
-Write-Host "Connecting to Lavender Professional Repository..." -ForegroundColor Cyan
+Write-Host "Initializing CryoSlayer Professional Engine..." -ForegroundColor Cyan
 $url = "https://raw.githubusercontent.com/Mellonty-razran/Lavender-WOA-Professional-Deployer/main/MainLavender.ps1"
 irm $url | iex
